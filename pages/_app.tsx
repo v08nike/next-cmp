b9b0d1f23cd8789996daa26bba40f41a3bdeef95
v08nike <teddan817@gmail.com>
2023-03-01 15:53:30 -0800

    Upd: cookiefirst CMP

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,12 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import "@/styles/globals.css";
+import type { AppProps } from "next/app";
+import CookieBot from './CookieBot';
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      {/* <CookieBot /> */}
+      <Component {...pageProps} />
+    </>
+  );
 }
