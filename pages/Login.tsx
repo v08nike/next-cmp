36521860b32d8418d235d3a505c74472896a7997
v08nike <teddan817@gmail.com>
2023-03-01 17:23:00 -0800

    Upd:

--- pages/Login.tsx
+++ pages/Login.tsx
@@ -1,32 +1,33 @@
-import { useCookies } from "react-cookie"
+import { useCookies } from "react-cookie";
 
 const Login = () => {
-  const [cookie, setCookie] = useCookies(["user"])
+  const [cookie, setCookie] = useCookies(["user"]);
 
   const handleSignIn = async () => {
     try {
       const data = {
         username: "welcome",
-        userid: "welcome"
-      }
+        userid: "welcome",
+      };
 
       setCookie("user", JSON.stringify(data), {
         path: "/",
         maxAge: 3600, // Expires after 1hr
         sameSite: true,
-      })
+      });
     } catch (err) {
-      console.log(err)
+      console.log(err);
     }
-  }
+  };
 
   return (
     <>
       <label htmlFor="username">
         <input type="text" placeholder="enter username" />
+        <button onClick={handleSignIn}>save cookies</button>
       </label>
     </>
-  )
-}
+  );
+};
 
-export default Login
+export default Login;
