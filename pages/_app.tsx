de1ec5660acff1e5105fb8dc88778688c1e45340
v08nike <teddan817@gmail.com>
2023-03-01 16:07:50 -0800

    Upd: _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,6 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
-import CookieBot from './CookieBot';
+// import CookieBot from './CookieBot';
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
